719bb4cb0b929f5394dbbeefd3ca5fe2ea1ca9a5
Saksham <2020UCP1801@mnit.ac.in>
2021-08-09 11:51:13 +0530

    getting video types available

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from pytube import YouTube
 import json
 
 with open("config.json") as f:
@@ -10,6 +11,33 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("index.html")
+
+
+@app.route('/get_types', methods=["POST"])
+def get_types():
+    # Getting Types of video based on entered url
+    if request.method == "POST":
+        try:
+            # Collecting data from form
+            url__ = request.form["urlInput"]
+            yt = YouTube(url__)
+            videos = list(yt.streams)
+            dict__ = {}
+            for index, video in enumerate(videos):
+                dict__[index + 1] = {}
+                str_video__ = str(video).split(" ")
+                for item in str_video__:
+                    if "mime_type" == item.split("=")[0]:
+                        dict__[index + 1]["mime_type"] = item.split("=")[1].split("\"")[1]
+                    if "res" == item.split("=")[0]:
+                        dict__[index + 1]["res"] = item.split("=")[1].split("\"")[1]
+                    if "type" == item.split("=")[0]:
+                        dict__[index + 1]["type"] = item.split("=")[1].split("\"")[1]
+                    if "abr" == item.split("=")[0]:
+                        dict__[index + 1]["abr"] = item.split("=")[1].split("\"")[1]
+            return jsonify(error=None, types=dict__)
+        except Exception as e:
+            return jsonify(error=str(e), types=None)
 
 
 if __name__ == '__main__':
--- static/script.js
+++ static/script.js
@@ -10,5 +10,22 @@ $(document).ready(function(){
         if($('#urlInput').val() == ""){
             showError("Enter URL");
         }
+        else{
+            showError("");
+            // Sending request to get types
+            $.ajax({
+                url: '/get_types',
+                method: 'POST',
+                data: $('#basicForm').serialize(),
+                success: function(res){
+                    if(res["error"] != null){
+                        showError(res["error"]);
+                    }
+                    else{
+                        console.log(res["types"]);
+                    }
+                }
+            });
+        }
     });
 });
